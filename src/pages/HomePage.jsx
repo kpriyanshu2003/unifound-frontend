5b9d8af17a2663aba43960c241d77c51c283e0cb
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2023-12-28 15:53:41 +0530

    Removed unused import

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import Wave from "../assets/Wave";
 import "../styles/Home.css";
 import HomeCard from "../Components/HomeCard";
 import Grid from "../Components/Grid";
