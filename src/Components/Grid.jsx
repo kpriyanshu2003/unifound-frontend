925dabc8b7b2e47057ed8ec26b0111e994d863f7
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2023-12-28 16:29:15 +0530

    Fixed Grid. Made responsive

--- src/Components/Grid.jsx
+++ src/Components/Grid.jsx
@@ -1,173 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/Grid.css";
 
 function Grid() {
+  const [num, setNum] = useState({ cols: 0, rows: 0 });
+
+  useEffect(() => {
+    function updateGridSize() {
+      const screenWidth = window.innerWidth;
+      const screenHeight = window.innerHeight;
+      const cols = Math.floor(screenWidth / 100) + 2;
+      const rows = Math.floor(screenHeight / 100);
+      setNum({ cols: cols, rows: rows });
+    }
+    updateGridSize();
+    window.addEventListener("resize", updateGridSize);
+    return () => {
+      window.removeEventListener("resize", updateGridSize);
+    };
+  }, []);
+  const generateGridCells = () => {
+    const totalCells = num.cols * num.rows;
+    const cells = [];
+    for (let i = 0; i < totalCells; i++) {
+      cells.push(<div key={i} className="grid-cell"></div>);
+    }
+    return cells;
+  };
   return (
-    <section>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
+    <section
+      id="grid-container"
+      style={{ gridTemplateColumns: `repeat(${num.cols}, 100px)` }}
+    >
+      {generateGridCells()}
     </section>
   );
 }
