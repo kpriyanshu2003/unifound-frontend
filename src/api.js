38ac42ae2bfdeb54c870abdf8b0312a71ff271ac
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2024-01-10 01:16:27 +0530

    Update API Links

--- src/api.js
+++ src/api.js
@@ -3,13 +3,19 @@ import axios from "axios";
 const API = axios.create({ baseURL: `http://localhost:3300/` });
 
 // fetch by category
-export const auth = (category) => API.get(`/items/?category=${category}`);
+// export const fetchItems = (category) => API.get(`/items/?category=${category}`);
+export const fetchItems = (category) =>
+  API.get(
+    `https://finding-nemo.onrender.com/lostItem/getByCategory?category=${category}`
+  );
 
 // fetch by id
 export const fetchItem = (id) => API.get(`/items/${id}`);
 
 // add new item
-export const createItem = (newItem) => API.post("/items/add", newItem);
+// export const createItem = (newItem) => API.post("/items/add", newItem);
+export const createItem = (newItem) =>
+  API.post("https://finding-nemo.onrender.com/lostItem/addLostItem", newItem);
 
 // remove item
 export const removeItem = (id) => API.delete(`/items/${id}`);
