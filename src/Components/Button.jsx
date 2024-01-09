6e1e74bfb17a430c84dbe83780d25efa7d298be7
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2024-01-10 00:37:48 +0530

    remove useless files

--- src/Components/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import '../styles/FoundCard.css'
-const Button = (props) => {
-  return (
-    <button className="upload_btn">{props.button}</button>
-  )
-}
-export default Button
